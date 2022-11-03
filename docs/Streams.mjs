618896650c95e3fb6ece3d048747ce50755a0caf
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-11-03 07:30:04 -0400

    Update Streams.mjs

--- docs/Streams.mjs
+++ docs/Streams.mjs
@@ -3,12 +3,77 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+import * as Types from "https://scotwatson.github.io/Debug/Types.mjs";
+import * as ErrorLog from "https://scotwatson.github.io/Debug/ErrorLog.mjs";
 import * as Queue from "https://scotwatson.github.io/Queue/Queue.mjs";
+
+// Push Source
+export class PushSource {
+  constructor() {
+  }
+  get recommendedTime() {
+  }
+  get bufferFull() {
+  }
+}
+
+export class PullSink {
+  constructor() {
+  }
+  get recommendedTime() {
+  }
+  get bufferEmpty() {
+  }
+}
+
+// Pull Source
+export class ReadableStreamSource extends self.EventTarget {
+  #readableStream;
+  constructor(args) {
+    try {
+      if (!(Types.isSimpleObject(args))) {
+        throw "Arguments must be a simple object.";
+      }
+      if (!(args.readableStream instanceof self.ReadableStream)) {
+        throw "readableStream must be of type self.ReadableStream.";
+      }
+      this.#readableStream = args.readableStream;
+      if (this.#readableStream.locked) {
+        throw "readableStream must be unlocked.";
+      }
+      this.#reader = this.#readableStream.getReader();
+    } catch (e) {
+    }
+  }
+  async pull(queue) {
+    let result;
+    try {
+      result = await this.#reader.read();
+    } catch (e) {
+      const evtError = new Event("error");
+      evtError.data = e;
+      this.dispatchEvent(evtError);
+      return;
+    }
+    if (result.done) {
+      this.#reader.releaseLock();
+      this.#reader = null;
+    }
+    queue.enqueue(result.value);
+  }
+};
+
+export class Sink {
+  constructor(args) {
+  }
+};
 
 export class Pipe extends self.EventTarget {
   #queue;
   #source;
+  #destination;
   constructor(args) {
+    
     this.#queue = new Queue.Queue();
     this.#source = args.source;
     this.#source.addEventListener("data-available", enqueuer);
