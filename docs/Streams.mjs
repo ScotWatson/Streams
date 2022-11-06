29a5d2438334bde6d73eab4f0fb7285dab5ef1c4
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-11-06 15:19:09 -0500

    Update Streams.mjs

--- docs/Streams.mjs
+++ docs/Streams.mjs
@@ -262,7 +262,7 @@ export class Pump {
   }
   async execute() {
     try {
-      if (!(Types.isInvocable(this.#puller)) {
+      if (!(Types.isInvocable(this.#puller))) {
         throw "Source must be non-null.";
       }
       const item = this.#puller.pull();
