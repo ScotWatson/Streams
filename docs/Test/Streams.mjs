65a94b20fbfcd54596cf996ba0f6a6984135a0cc
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-12-25 08:14:01 -0500

    Update Streams.mjs

--- docs/Test/Streams.mjs
+++ docs/Test/Streams.mjs
@@ -315,23 +315,32 @@ export class AsyncByteReaderPushSource {
   #buffer;
   constructor(args) {
     try {
+      console.log(1);
       if (!("callback" in args)) {
         throw "Argument \"callback\" must be provided.";
       }
+      console.log(2);
       this.#callback = args.callback;
+      console.log(3);
       const taskFunction = Tasks.createStatic({
         function: this.#task,
         this: this,
       });
+      console.log(4);
       const taskCallbackController = new Tasks.CallbackController({
         invoke: taskFunction,
       });
+      console.log(5);
       this.#taskCallback = taskCallbackController.callback;
+      console.log(6);
       this.#pushCallback = null;
+      console.log(7);
       this.#offset = 0;
+      console.log(8);
       this.#buffer = new Memory.Block({
         byteLength: this.#chunkByteLength,
       });
+      console.log(9);
       this.#taskCallback.invoke();
     } catch (e) {
       ErrorLog.rethrow({
@@ -505,36 +514,26 @@ export class ReadableByteStreamPushSource {
           return args;
         }
       })();
-      console.log(1);
       if (!(readableStream instanceof self.ReadableStream)) {
         throw "Argument \"readableStream\" must be of type self.ReadableStream.";
       }
-      console.log(2);
       if (readableStream.locked) {
         throw "Argument \"readableStream\" must be unlocked.";
       }
-      console.log(3);
       const reader = readableStream.getReader();
-      console.log(4);
       this.#reader = reader;
-      console.log(5);
       const callbackFunction = Tasks.createStatic({
         function: this.#process,
         this: this,
       });
-      console.log(6);
       const callbackController = new Tasks.CallbackController({
         invoke: callbackFunction,
       });
-      console.log(7);
       this.#pushSourceController = new AsyncByteReaderPushSource({
         callback: callbackController.callback,
       });
-      console.log(8);
       this.#closedSignalController = new SignalController();
-      console.log(9);
       this.#cancelledSignalController = new SignalController();
-      console.log(10);
       reader.closed.then(function () {
         this.#closedSignalController.dispatch();
         this.#cancelledSignalController.dispatch();
