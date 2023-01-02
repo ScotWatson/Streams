dbc3f1765ee392d2fa758b17db677b88ac666e56
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-01-02 11:24:20 -0500

    Update Streams.mjs

--- docs/Test/Streams.mjs
+++ docs/Test/Streams.mjs
@@ -1,5 +1,5 @@
 /*
-(c) 2022 Scot Watson  All Rights Reserved
+(c) 2023 Scot Watson  All Rights Reserved
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -2740,13 +2740,13 @@ export class AsyncPushSourceNode {
       }
       const start = self.performance.now();
       const that = this;
+      this.#promise = this.#asyncSource.execute({
+        state: this.#state,
+      });
       self.setTimeout(function () {
         that.#promise.then(that.#staticExecute);
       }, this.#interval);
       this.#outputCallback.invoke(outputItem);
-      this.#promise = this.#asyncSource.execute({
-        state: this.#state,
-      });
       const end = self.performance.now();
       // Statistics
       this.#avgInterval *= (1 - this.#smoothingFactor);
